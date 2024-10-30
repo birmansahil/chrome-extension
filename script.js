e944c5f35cab37a60bbfdea6e3e078551e688a68
birmansahil <contact.birmansahil@gmail.com>
2024-10-29 22:00:56 -0400

    added functionality to store input and render saved leads in list format

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
-function saveLead() {
-    console.log("Btn clicked");
+const inputEl = document.querySelector("#input-el");
+const inputBtn = document.querySelector("#input-btn");
+const ulEl = document.querySelector("#ul-el");
+
+let myLeads = [];
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value);
+    inputEl.value = "";
+    renderLeads();
+});
+
+function renderLeads() {
+    let listItems = "";
+
+    for (let i = 0; i < myLeads.length; i ++) {
+        listItems += `
+            <li>
+                <a target='_blank' href='${myLeads[i]}'>${myLeads[i]}</a>
+            </li>
+        `;
+    }
+
+    ulEl.innerHTML = listItems;
 }
